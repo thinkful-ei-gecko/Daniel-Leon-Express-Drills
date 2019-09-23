8229717afad886ae1b82e0c398ba664d25382d5e
Daniel <herrnichols@gmail.com>
2019-09-23 16:28:10 -0400

    finished drill 2

--- app.js
+++ app.js
@@ -7,21 +7,44 @@ app.listen(8000, () => {
 
 //Drill 1 
 
-app.get('/sum', (req, res) => {
-  let a = req.query.a;
-  let b = req.query.b;
+// app.get('/sum', (req, res) => {
+//   let a = req.query.a;
+//   let b = req.query.b;
 
-  if (!a || !b) {
-    return res.status(400).send('Values a and b are required');
+//   if (!a || !b) {
+//     return res.status(400).send('Values a and b are required');
+//   }
+
+//   let aInt = parseInt(a);
+//   let bInt = parseInt(b);
+//   let c = aInt + bInt;
+
+//   if (typeof aInt !== Number || typeof bInt !== Number) {
+//     return res.status(400).send('Needs to be a number');
+//   }
+
+//   res.send(`The sum of ${aInt} and ${bInt} equals ${c}.`);
+// });
+
+
+//Drill 2 
+
+app.get('/cipher', (req,res) => {
+  let text = req.query.text;
+  let shift = req.query.shift;
+  let parseShift = parseInt(shift);
+  if (typeof parseShift !== 'number' || !parseShift) { return res.status(400).send('Shift must be a number'); }
+  else {
+    let charArray = text.split('');
+    charArray.forEach((letter,index) => {
+      console.log(charArray[index].charCodeAt(0));
+      if (charArray[index].charCodeAt(0) >= 65 && charArray[index].charCodeAt(0) <= 127 || charArray[index].charCodeAt(0) === 32) {
+        charArray[index] = String.fromCharCode(charArray[index].charCodeAt(0) + parseShift);
+      }
+      else {
+        return res.status(400).send('Must be a letter between character codes 65-127');
+      }
+    });
+    res.send(`${charArray}`);
   }
-
-  let aInt = parseInt(a);
-  let bInt = parseInt(b);
-  let c = aInt + bInt;
-
-  if (typeof aInt !== Number || typeof bInt !== Number) {
-    return res.status(400).send('Needs to be a number');
-  }
-
-  res.send(`The sum of ${aInt} and ${bInt} equals ${c}.`);
 });
